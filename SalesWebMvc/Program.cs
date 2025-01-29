c5b3a0f29595246631e051ff5cd508ec5cdbf93e
Thaleson Eduardo <thalison1230@gmail.com>
2025-01-29 19:52:07 -0300

    Implementing simple search

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -17,6 +17,7 @@ b => b.MigrationsAssembly("SalesWebMvc")));
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<SalesRecordService>();
 
 
 // Add services to the container.
